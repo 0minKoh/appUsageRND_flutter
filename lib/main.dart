6efe3850244362db922787173828151834c7c2c7
0minKoh <esesang13@naver.com>
2024-09-12 17:48:37 +0900

    rename home.dart to app_usage_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // main.dart
-import 'package:app_usage_rnd_android/screens/home.dart';
+import 'package:app_usage_rnd_android/screens/app_usage_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
