5ef086084ba79889648e36c3d9204ac53ae79861
0minKoh <esesang13@naver.com>
2024-09-12 21:54:49 +0900

    local_push_notification android setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 // main.dart
 import 'package:app_usage_rnd_android/screens/app_usage_screen.dart';
+import 'package:app_usage_rnd_android/screens/notification_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(LocalNotification());
 }
---
+++ lib/screens/notification_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class LocalNotification extends StatefulWidget {
+  const LocalNotification({super.key});
+
+  @override
+  State<LocalNotification> createState() => _LocalNotificationState();
+}
+
+class _LocalNotificationState extends State<LocalNotification> {
+  final FlutterLocalNotificationsPlugin _local =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _permissionWithNotification();
+    _initialization();
+  }
+
+  void _permissionWithNotification() async {
+    await [Permission.notification].request();
+  }
+
+  void _initialization() async {
+    AndroidInitializationSettings android =
+        const AndroidInitializationSettings("@mipmap/ic_launcher");
+    InitializationSettings settings = InitializationSettings(android: android);
+    await _local.initialize(settings);
+
+    // 알림 채널 생성
+    const AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'show_test', // 채널 ID
+      'Test Notifications', // 채널 이름
+      description: 'This channel is used for test notifications.',
+      importance: Importance.max,
+    );
+
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    // 알림 채널 등록
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.createNotificationChannel(channel);
+  }
+
+  void _show() async {
+    NotificationDetails details = const NotificationDetails(
+      android: AndroidNotificationDetails(
+        "show_test",
+        "show_test",
+        channelDescription: "Test Local notications",
+        importance: Importance.max,
+        priority: Priority.high,
+      ),
+    );
+    await _local.show(
+      0,
+      "타이틀이 보여지는 영역입니다.",
+      "컨텐츠 내용이 보여지는 영역입니다.\ntest show()",
+      details,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Local Notification"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _show,
+                child: const Text("노티 보이기"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
